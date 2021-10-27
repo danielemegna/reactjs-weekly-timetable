c24e07d2e94f84aa5f9f19856ff3c147bd1b5f78
Daniele Megna <daniele.megna@casavo.com>
2021-10-27 09:03:11 +0200

    Hide AuthenticationModal on click and show error message on login
    currently any username is invalid :)

--- src/components/AuthenticationModal.tsx
+++ src/components/AuthenticationModal.tsx
@@ -1,14 +1,37 @@
+import { error } from 'console'
+import { useState } from 'react'
 import style from './AuthenticationModal.module.scss'
 
 export default function AuthenticationModal() {
+
+	const [toBeShown, setToBeShown] = useState<boolean>(true)
+	const [invalidUsername, setInvalidUsername] = useState<boolean>(false)
+
+	if (!toBeShown)
+		return <></>
+
 	return (
 		<div className={style.overlay}>
 			<form>
 				<h3>🐱 Dimmi chi sei!</h3>
 				<input type="text" />
-				<br/><br/>
-				<button type="button" custom-color="blue">Entra</button>
-				<button type="button" custom-color="yellow">Fammi solo guardare</button>
+				<br />
+				{invalidUsername && (<label className={style.errormessage}>Username non valido!</label>)}
+				<br />
+				<button
+					type="button"
+					custom-color="blue"
+					onClick={() => setInvalidUsername(true)}
+				>
+					Entra
+				</button>
+				<button
+					type="button"
+					custom-color="yellow"
+					onClick={() => setToBeShown(false)}
+				>
+					Fammi solo guardare
+				</button>
 			</form>
 		</div>
 	)
