814ba6ce2682591c9b3549510575f83017e6566a
Daniele Megna <daniele.megna@casavo.com>
2021-11-05 18:39:54 +0100

    Convert tab indentation to spaces !

--- src/components/AuthenticationModal.tsx
+++ src/components/AuthenticationModal.tsx
@@ -4,35 +4,35 @@ import style from './AuthenticationModal.module.scss'
 
 export default function AuthenticationModal() {
 
-	const [toBeShown, setToBeShown] = useState<boolean>(true)
-	const [invalidUsername, setInvalidUsername] = useState<boolean>(false)
+  const [toBeShown, setToBeShown] = useState<boolean>(true)
+  const [invalidUsername, setInvalidUsername] = useState<boolean>(false)
 
-	if (!toBeShown)
-		return <></>
+  if (!toBeShown)
+    return <></>
 
-	return (
-		<div className={style.overlay}>
-			<form>
-				<h3>🐱 Dimmi chi sei!</h3>
-				<input type="text" />
-				<br />
-				{invalidUsername && (<label className={style.errormessage}>Username non valido!</label>)}
-				<br />
-				<button
-					type="button"
-					custom-color="blue"
-					onClick={() => setInvalidUsername(true)}
-				>
-					Entra
-				</button>
-				<button
-					type="button"
-					custom-color="yellow"
-					onClick={() => setToBeShown(false)}
-				>
-					Fammi solo guardare
-				</button>
-			</form>
-		</div>
-	)
+  return (
+    <div className={style.overlay}>
+      <form>
+        <h3>🐱 Dimmi chi sei!</h3>
+        <input type="text" />
+        <br />
+        {invalidUsername && (<label className={style.errormessage}>Username non valido!</label>)}
+        <br />
+        <button
+          type="button"
+          custom-color="blue"
+          onClick={() => setInvalidUsername(true)}
+        >
+          Entra
+        </button>
+        <button
+          type="button"
+          custom-color="yellow"
+          onClick={() => setToBeShown(false)}
+        >
+          Fammi solo guardare
+        </button>
+      </form>
+    </div>
+  )
 }
