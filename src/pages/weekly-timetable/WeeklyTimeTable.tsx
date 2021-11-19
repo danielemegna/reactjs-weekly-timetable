ed4ceec5b6781ae8fc64f91dabf0e4ffd440a3f5
Daniele Megna <daniele.megna@casavo.com>
2021-11-19 08:22:23 +0100

    Add AnimalEmergency logo as top logo in WeeklyTimeTable

--- src/pages/weekly-timetable/WeeklyTimeTable.tsx
+++ src/pages/weekly-timetable/WeeklyTimeTable.tsx
@@ -1,6 +1,7 @@
 import moment from "moment";
 import 'moment/locale/it'
 import { useState } from 'react';
+import ae_logo from './ae_logo.jpg'
 import AuthenticationModal from "../../components/authentication-modal/AuthenticationModal";
 import { ChangeWeekButton, Direction as ChangeWeekButtonDirection } from "../../components/change-week-button/ChangeWeekButton";
 import { default as WeeklyTimeTableComponent } from '../../components/weekly-timetable/WeeklyTimeTable';
@@ -12,6 +13,7 @@ export default function WeeklyTimeTable() {
   return (
     <div style={{ display: "inline-block", padding: "0.5em" }} >
       <AuthenticationModal />
+      <TopLogo />
       <WeeklyTimeTableComponent startOfWeek={startOfWeek} />
       <ChangeWeekButton
         direction={ChangeWeekButtonDirection.PREVIOUS}
@@ -21,5 +23,13 @@ export default function WeeklyTimeTable() {
         onClick={() => setStartOfWeek(startOfWeek.clone().add(1, 'week'))}
       />
     </div>
+  )
+}
+
+function TopLogo() : JSX.Element {
+  return (
+      <div style={{textAlign: "center", paddingBottom: "4px"}}>
+        <img style={{ width: "8em" }} src={ae_logo} />
+      </div>
   )
 }
