3ed4022be9ca685ae8775154e79bf0948107636f
Daniele Megna <daniele.megna@casavo.com>
2021-11-22 08:59:54 +0100

    Implement dummy GetWeekShifts route and usecase

---
+++ backend/src/GetWeekShiftsUseCase.ts
@@ -0,0 +1,101 @@
+type WeekShifts  = {
+  date: String
+  shifts: Shift[]
+}
+
+type Shift = {
+  date: String
+  morning: Person[]
+  afternoon: Person[]
+}
+
+type Person = {
+  name: String
+  color: String
+}
+
+const GetWeekShiftsUseCase = (date: String) : WeekShifts => {
+    console.log(date)
+    return {
+      date: date,
+      shifts: [
+        {
+          date: '2021-11-22',
+          morning: [
+            {name: 'Anna', color: 'default'},
+            {name: 'Giuliana', color: 'default'}
+          ],
+          afternoon: [
+            {name: 'Monica', color: 'default'},
+            {name: 'Roby', color: 'default'},
+            {name: 'Alessia', color: 'default'}
+          ]
+        },
+        {
+          date: '2021-11-23',
+          morning: [
+            {name: 'Cristina', color: 'default'},
+            {name: 'Eleonora', color: 'default'},
+            {name: 'Giulia', color: 'default'}
+          ],
+          afternoon: [
+            {name: 'Alessia', color: 'default'},
+            {name: 'Grazia', color: 'default'}
+          ]
+        },
+        {
+          date: '2021-11-24',
+          morning: [
+            {name: 'Sonia', color: 'green'},
+            {name: 'Anna', color: 'default'},
+            {name: 'Simone', color: 'default'},
+            {name: 'Giulia in affiancamento', color: 'blue'}
+          ],
+          afternoon: [ ]
+        },
+        {
+          date: '2021-11-25',
+          morning: [
+            {name: 'Cristiana', color: 'default'},
+            {name: 'Giuliana', color: 'default'}
+          ],
+          afternoon: [
+            {name: 'Alice', color: 'default'}
+          ]
+        },
+        {
+          date: '2021-11-26',
+          morning: [
+            {name: 'Anna', color: 'default'},
+          ],
+          afternoon: [
+            {name: 'Ylenia + Mamy', color: 'default'}
+          ]
+        },
+        {
+          date: '2021-11-27',
+          morning: [
+            {name: 'Gaia', color: 'default'},
+            {name: 'Laura e Rina in affiancamento', color: 'blue'},
+          ],
+          afternoon: [
+            {name: 'Daniele', color: 'default'},
+            {name: 'Paola', color: 'default'}
+          ]
+        },
+        {
+          date: '2021-11-28',
+          morning: [
+            {name: 'Barbara', color: 'default'},
+            {name: 'Laura', color: 'default'},
+            {name: 'Erica in affiancamento', color: 'blue'}
+          ],
+          afternoon: [
+            {name: 'Monica P', color: 'default'}
+          ]
+        },
+      ]
+    }
+}
+
+export default GetWeekShiftsUseCase
--- backend/start.ts
+++ backend/start.ts
@@ -1,17 +1,21 @@
 import http = require('http');
+import GetWeekShiftsUseCase from './src/GetWeekShiftsUseCase';
 
 http.createServer((request, response) => {
-  console.log(request.url)
-  switch(request.url) {
-    case '/': {
-      response.writeHead(200, { 'Content-Type': 'text/plain' })
-      response.end('Hello, world!', 'utf-8')
-      break;
-    }
-    default: {
-      response.writeHead(500)
-      response.end('Male!')
-      break;
-    }
+  const {url, method} = request
+  console.log(`Received ${method} on ${url}`)
+  if(method == 'GET' && url == '/') {
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.end('Hello, world!', 'utf-8')
+    return
   }
+  if(method == 'GET' && url?.startsWith('/week/')) {
+    const date = url.split('/')[2]
+    const shifts = GetWeekShiftsUseCase(date)
+    response.writeHead(200, { 'Content-Type': 'application/javascript' })
+    response.end(JSON.stringify(shifts))
+    return
+  }
+  response.writeHead(500)
+  response.end('Male!')
 }).listen(8125);
