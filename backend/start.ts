3d8c238192b9a8986e9fc42fc7e5943c25fff502
Daniele Megna <daniele.megna@casavo.com>
2022-09-13 14:34:59 +0200

    Starting implement read body from request

--- backend/start.ts
+++ backend/start.ts
@@ -8,6 +8,13 @@ http.createServer((request: IncomingMessage, response: ServerResponse) => {
     const { url, method, headers: { origin } } = request
     console.log(`Received ${method} on ${url}`)
 
+    let receivedData = '';
+    request.on('data', chunk => receivedData += chunk);
+    request.on('end', () => {
+      const requestBody = parseRequestBody(receivedData)
+      console.log(requestBody) // TODO
+    });
+
     if (method == 'GET' && url == '/') {
       response.writeHead(200, { 'Content-Type': 'text/plain' })
       response.end('Hello, world!', 'utf-8')
@@ -43,6 +50,21 @@ http.createServer((request: IncomingMessage, response: ServerResponse) => {
     emptyResponse(500, response, origin)
   }
 }).listen(8125);
+
+function parseRequestBody(receivedData: string): string | undefined {
+  if(!receivedData || receivedData == '') {
+    console.log('Empty request body')
+    return undefined
+  }
+
+  try {
+    const requestBody = JSON.parse(receivedData)
+    console.log(`Received request body: ${JSON.stringify(requestBody)}`)
+  } catch(error) {
+    console.log('Invalid request body')
+    return undefined
+  }
+}
 
 function parseDate(dateString: string): Moment {
   // TODO merge regex and moment validation
