fb2d64e2e4f75e16d17051665fef94a07e437566
Daniele Megna <daniele.megna@casavo.com>
2022-09-13 11:54:03 +0200

    Destructure origin header in root createServer
    ... instead of pass entire IncomingMessage to inner functions
    that only need the origin header value for CORS

--- backend/start.ts
+++ backend/start.ts
@@ -5,7 +5,7 @@ import GetWeekShiftsUseCase from './src/GetWeekShiftsUseCase';
 
 http.createServer((request: IncomingMessage, response: ServerResponse) => {
   try {
-    const { url, method } = request
+    const { url, method, headers: { origin } } = request
     console.log(`Received ${method} on ${url}`)
 
     if (method == 'GET' && url == '/') {
@@ -16,31 +16,31 @@ http.createServer((request: IncomingMessage, response: ServerResponse) => {
     if (method == 'GET' && url?.startsWith('/week/')) {
       const date = parseDate(url.split('/')[2])
       const shifts = GetWeekShiftsUseCase(date)
-      jsonResponseWith(shifts, 200, response, request);
+      jsonResponseWith(shifts, 200, response, origin);
       return
     }
     if (url?.startsWith('/togglePresence/')) {
-      switch(method) {
+      switch (method) {
         case 'OPTIONS':
-          emptyResponse(204, response, request)
+          emptyResponse(204, response, origin)
           return
         case 'POST':
           const date = parseDate(url.split('/')[2])
           console.log('Toggling presence!', date)
           //TogglePresenceUseCase(date)
-          emptyResponse(201, response, request)
+          emptyResponse(201, response, origin)
           return
       }
       console.log('Method not allowed!')
-      emptyResponse(405, response, request)
+      emptyResponse(405, response, origin)
       return
     }
 
     console.log('Route not found!')
-    emptyResponse(404, response, request)
+    emptyResponse(404, response, origin)
   } catch (error) {
     console.log('Error during request handling!', error)
-    emptyResponse(500, response, request)
+    emptyResponse(500, response, origin)
   }
 }).listen(8125);
 
@@ -53,32 +53,28 @@ function parseDate(dateString: string): Moment {
   return moment(dateString)
 }
 
-function jsonResponseWith(body: object, statusCode: number, response: ServerResponse, request: IncomingMessage) {
+function jsonResponseWith(body: object, statusCode: number, response: ServerResponse, origin?: string) {
   response.writeHead(statusCode, {
     'Content-Type': 'application/json',
-    ...CORSAndCacheHeaders(request),
+    ...CORSAndCacheHeaders(origin),
   })
   response.end(JSON.stringify(body));
 }
 
-function emptyResponse(statusCode: number, response: ServerResponse, request: IncomingMessage) {
-  response.writeHead(statusCode, CORSAndCacheHeaders(request))
+function emptyResponse(statusCode: number, response: ServerResponse, origin?: string) {
+  response.writeHead(statusCode, CORSAndCacheHeaders(origin))
   response.end();
 }
 
-function CORSAndCacheHeaders(request: IncomingMessage) {
+function CORSAndCacheHeaders(origin?: string) {
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://md.tru.io:3000'
+  ]
+  const isOriginAllowed = origin ? allowedOrigins.includes(origin) : false
   return {
-    'Access-Control-Allow-Origin': accessControlAllowOriginFor(request),
+    'Access-Control-Allow-Origin': isOriginAllowed ? origin : '',
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     'Cache-Control': 'must-revalidate,no-cache,no-store',
   }
-}
-
-function accessControlAllowOriginFor(request: IncomingMessage): string {
-      const origin = request.headers?.origin ?? 'unknown'
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'http://md.tru.io:3000'
-      ]
-      return allowedOrigins.includes(origin) ? origin : ''
 }
