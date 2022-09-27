9d9ac69b8a1a6af68a67b71515309aa0094a7482
Daniele Megna <daniele.megna@casavo.com>
2022-09-27 10:38:49 +0200

    fix: return requestBody from parseRequestBody function

--- backend/start.ts
+++ backend/start.ts
@@ -12,7 +12,6 @@ http.createServer((request: IncomingMessage, response: ServerResponse) => {
     request.on('data', chunk => receivedData += chunk);
     request.on('end', () => {
       const requestBody = parseRequestBody(receivedData)
-      console.log(requestBody) // TODO
     });
 
     if (method == 'GET' && url == '/') {
@@ -60,6 +59,7 @@ function parseRequestBody(receivedData: string): string | undefined {
   try {
     const requestBody = JSON.parse(receivedData)
     console.log(`Received request body: ${JSON.stringify(requestBody)}`)
+    return requestBody
   } catch(error) {
     console.log('Invalid request body')
     return undefined
